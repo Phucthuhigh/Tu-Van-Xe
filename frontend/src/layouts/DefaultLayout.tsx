d23ba80df65bf813b388cb65518bec09fe3d5f88
phucthuhigh <phucgamingpro@gmail.com>
2024-06-15 11:02:53 +0700

    layout, theme

--- frontend/src/layouts/DefaultLayout.tsx
+++ frontend/src/layouts/DefaultLayout.tsx
@@ -1,5 +1,11 @@
 const DefaultLayout = ({ children }: { children: React.ReactNode }) => {
-	return <div>{children}</div>
+	return (
+		<div>
+			<header>HEADER</header>
+			{children}
+			<div>FOOTER</div>
+		</div>
+	)
 }
 
 export default DefaultLayout
